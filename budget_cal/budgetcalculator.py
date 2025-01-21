c30bfca1aff324c5239a6b0f8cf10384746f3488
jpb1991 <jpb6891@gmail.com>
2025-01-21 16:02:51 -0500

    Update and rename New Text Document.txt to budgetcalculator.py
    Signed-off-by: jpb1991 <jpb6891@gmail.com>

---
+++ budget_cal/budgetcalculator.py
@@ -0,0 +1,6 @@
+total_money = 400.25
+work_money = 300.14
+spending_money = 29.99
+utility_money = 100.20
+leftover_money = total_money + work_money - spending_money - utility_money
+print("${:.2f} is how much money that is left over.".format(leftover_money))
