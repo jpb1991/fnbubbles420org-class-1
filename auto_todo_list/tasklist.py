8fbbdd0ca4b88a185a8e4df8651bd39b478f5996
jpb1991 <jpb6891@gmail.com>
2025-01-28 13:24:56 -0500

    Update tasklist.py
    Signed-off-by: jpb1991 <jpb6891@gmail.com>

--- auto_todo_list/tasklist.py
+++ auto_todo_list/tasklist.py
@@ -1,53 +1,12 @@
-# The following code with edits to the original code came from https://www.youtube.com/watch?v=1XDZ9Dy6qo4
-tasks = []
+task_list = []
 
-def add_task():
-    # add a task
-    
-    task = input("Enter task: ")
-    tasks.append(task)
-    print("The task has been added.")
+def add_task(task_title, description):
+    tasks_list.append({"title": task_title, "describe": description})
 
-def view_tasks():
-    # view the tasks
-    if len(tasks==0:
-        print("Tasks are not currently available.")
-    else:
-        print('The tasks are:')
-        for i, task in enumerate(tasks):
-            print(f'{i+1}. {task}')
-def remove_task():
-    # remove a task
-    if len(tasks==0:
-        print("Tasks are not currently available to be removed.")
-    else:
-        print('The tasks are:')
-        for i, task in enumerate(tasks):
-            print(f'{i+1}. {task}')
-        choice = int(input('Enter the task number to remove it:'))
-        if 0 < choice <= len(tasks):
-            del tasks[choice-1]
-            print('The task has been removed.')
-        else:
-            print('Please make another selection.')
+def list_tasks():
+    for task in tasks_list:
+        print(f"Applied to {task['title']} at {task['describe']}")
 
-def main():
-    while True:
-        print("1. Add a task.")
-        print("2. View the tasks.")
-        print("3. Remove a task.")
-        print("4. Exit.")
-        choice = int(input("Make a selection."))
-        if choice == 1:
-            add_task()
-        elif == 2:
-            view_tasks()
-        elif == 3:
-            remove_task()
-        elif == 4:
-            print("Goodbye.")
-            break
-        else:
-            print('Please make another selection.')
-if __name__ == "__main__":
-    main()
+add_task("1. Write programs", "Code three programs entirely from scratch.")
+add_task("2. Create README.MD file", "Create README.MD file on github.")
+list_tasks()
